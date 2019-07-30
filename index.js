12f6a4374ac515f709759c8021d8a5b1a2b3998b
Mike Kurdizel <michael.kurdziel@gmail.com>
2019-07-30 15:12:36 -0700

    Adding checkin publish for keepalive checking`

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const _ = require('lodash');
+const moment = require('moment');
 const winston = require('winston');
 const { combine, timestamp, label, printf } = winston.format;
 const mqtt = require('mqtt')
@@ -11,9 +12,10 @@ const NODE_NAME_RELAY_2 = 'relay_2';
 const GPIO_NUM_RELAY_1 = 20;
 const GPIO_NUM_RELAY_2 = 21;
 
-const MQTT_HOST = 'mqtt://192.168.1.2';
-const MQTT_TOPIC = 'node/gardenpi';
+const MQTT_HOST = process.env.MQTT_HOST || 'mqtt://192.168.1.2';
+const MQTT_TOPIC = process.env.MQTT_TOPIC || 'node/gardenpi';
 const MQTT_TOPIC_SET = 'set';
+const MQTT_TOPIC_CHECKIN = 'checkin';
 
 const MQTT_TOPIC_RELAY_1 = `${MQTT_TOPIC}/${NODE_NAME_RELAY_1}`;
 const MQTT_TOPIC_RELAY_2 = `${MQTT_TOPIC}/${NODE_NAME_RELAY_2}`;
@@ -22,6 +24,9 @@ const MQTT_TOPIC_SET_RELAY_2 = `${MQTT_TOPIC_RELAY_2}/${MQTT_TOPIC_SET}`;
 
 const MQTT_VALUE_ON = 'ON';
 const MQTT_VALUE_OFF = 'OFF';
+
+// Period at which to update the checkin time
+const CHECKIN_PERIOD_MS = 5000;
 
 // Interval at which to send the current relay status back
 const MQTT_STATUS_UPDATE_INTERVAL_MS = 5000;
@@ -49,6 +54,7 @@ PORT_1 = new Gpio(GPIO_NUM_RELAY_1, 'out');
 PORT_2 = new Gpio(GPIO_NUM_RELAY_2, 'out');
 
 const statusInterval = setInterval(sendStatus, MQTT_STATUS_UPDATE_INTERVAL_MS); 
+const checkinInterval = setInterval(sendCheckin, CHECKIN_PERIOD_MS); 
 
 client.on('connect', () => {
 	LOGGER.info(`Connected to MQTT host: ` + MQTT_HOST);
@@ -90,5 +96,10 @@ function switchRelay(mqttTopic, port, name, message) {
      }
 
     sendRelayStatus(mqttTopic, port);
+}
+
+function sendCheckin() {
+    LOGGER.debug(`Sending checkin: ${moment().format()}`);
+    client.publish(`${MQTT_TOPIC}/${MQTT_TOPIC_CHECKIN}`, moment().format());
 }
 
